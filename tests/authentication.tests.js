aab9ec91b4e585bea5912061a8dd1625c9f024b1
Jac Sebregts <jac.sebregts@student.avans.nl>
2018-05-16 22:13:18 +0200

    Added 'mocha'

--- tests/authentication.tests.js
+++ tests/authentication.tests.js
@@ -4,6 +4,7 @@
 const db = require("../config/db");
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const mocha = require('mocha');
 const server = require('../server');
 
 let should = chai.should();
--- tests/meals.tests.js
+++ tests/meals.tests.js
@@ -2,6 +2,7 @@
 const db = require("../config/db");
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const mocha = require('mocha');
 const server = require('../server');
 
 let should = chai.should();
